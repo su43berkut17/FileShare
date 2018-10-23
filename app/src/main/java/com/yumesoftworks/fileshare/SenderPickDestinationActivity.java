8539ef2b53c477cdf3d603f1aa854665d58e8b53
simbanaf <p7477pp7477p>
2018-10-23 16:30:07 -0700

    p2p Broadcast receiver implemented

--- app/src/main/java/com/yumesoftworks/fileshare/SenderPickDestinationActivity.java
+++ app/src/main/java/com/yumesoftworks/fileshare/SenderPickDestinationActivity.java
@@ -1,20 +1,45 @@
 package com.yumesoftworks.fileshare;
 
+import android.content.Context;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.WifiP2pManager.Channel;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SenderPickDestinationActivity extends AppCompatActivity {
+
+    private final IntentFilter intentFilter=new IntentFilter();
+    private Channel mChannel;
+    private WifiP2pManager mManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sender_pick_destination);
 
-        //create the connection
+        //create the p2p connection
         createConnection();
     }
 
     private void createConnection(){
+        //create the intent filters
+        //Indicates a change in the Wi-Fi Peer-to-Peer status.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
 
+        //ndicates a change in the list of available peers.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+
+        //Indicates the state of Wi-Fi P2P connectivity has changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+
+        //Indicates this device's details have changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, getMainLooper(), null);
     }
+
+
 }
