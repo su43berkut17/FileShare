33cef24a4e29fb39de17ba8121bd1cae547915c6
simbanaf <p7477pp7477p>
2018-11-08 17:25:47 -0800

    Done Json avatar test CreatingAvatarTest WIP Added contrib espresso to gradle

--- app/src/androidTest/java/com/yumesoftworks/fileshare/CreatingAvatarTest.java
+++ app/src/androidTest/java/com/yumesoftworks/fileshare/CreatingAvatarTest.java
@@ -1,4 +1,33 @@
 package com.yumesoftworks.fileshare;
 
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+import androidx.test.rule.ActivityTestRule;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4ClassRunner.class)
 public class CreatingAvatarTest {
+    @Rule
+    public ActivityTestRule<WelcomeScreenActivity> mActivityRule = new ActivityTestRule<>(WelcomeScreenActivity.class);
+
+    @Test
+    public void saveChanges(){
+        //select 1st avatar
+        onView(withId(R.id.rv_aws_avatars)).perform(RecyclerViewActions.actionOnItemAtPosition(0,click()));
+
+        //type the username
+
+        //press the button
+
+        //load database and check if it saved the 2 changes
+
+    }
 }
--- app/src/androidTest/java/com/yumesoftworks/fileshare/JsonAvatarParserTest.java
+++ app/src/androidTest/java/com/yumesoftworks/fileshare/JsonAvatarParserTest.java
@@ -5,23 +5,26 @@ import android.content.Context;
 import com.yumesoftworks.fileshare.data.AvatarAndVersion;
 import com.yumesoftworks.fileshare.utils.JsonAvatarParser;
 
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
-@RunWith(AndroidJUnit4.class)
+import static org.junit.Assert.assertTrue;
+
+@RunWith(AndroidJUnit4ClassRunner.class)
 public class JsonAvatarParserTest{
     @Rule
     public ActivityTestRule<WelcomeScreenActivity> mActivityRule = new ActivityTestRule<>(WelcomeScreenActivity.class);
+    public Context mContext=InstrumentationRegistry.getInstrumentation().getTargetContext();
 
     @Test
     public void test_connection(){
-        JsonAvatarParser mAvatarParser=new JsonAvatarParser(mActivityRule.getActivity().getApplicationContext());
+        //JsonAvatarParser mAvatarParser=new JsonAvatarParser(mActivityRule.getActivity().getApplicationContext());
+        JsonAvatarParser mAvatarParser=new JsonAvatarParser(mContext);
 
         //we load the parser
         mAvatarParser.loadData();
@@ -34,21 +37,38 @@ public class JsonAvatarParserTest{
 
         AvatarAndVersion test=mAvatarParser.getAvatarAndVersionTest();
 
-        //check data inside avatar an version
+        //we check if its loaded
+        if (test==null){
+            //if it didnt load it might mean there is no internet connection
+            assertTrue(test!=null);
+        }else{
+            //we check the data that we received from the json
+            assertTrue(test.getVersion()==1);
 
+            assertTrue(test.getAvatarList().get(0).getPath()=="https://www.yumesoftworks.com/fileshare/avatars/avatar_9.png");
+        }
     }
 
     @Test
+
     public void TestParserMockData(){
         //mock json
-        String mockJSON="";
+        String mockJSON="{\n" +
+                "\t\"version\": 1,\n" +
+                "\t\"items\":[\n" +
+                "\t\t{\"url\":\"https://www.yumesoftworks.com/fileshare/avatars/avatar_9.png\"},\n" +
+                "\t\t{\"url\":\"https://www.yumesoftworks.com/fileshare/avatars/avatar_10.png\"}\n" +
+                "\t]\n" +
+                "}";
 
         //we check if the json has been updated
-        JsonAvatarParser mAvatarParser=new JsonAvatarParser(mActivityRule.getActivity().getApplicationContext());
+        //JsonAvatarParser mAvatarParser=new JsonAvatarParser(mActivityRule.getActivity().getApplicationContext());
+        JsonAvatarParser mAvatarParser=new JsonAvatarParser(mContext);
 
         AvatarAndVersion test=mAvatarParser.getAvatarAndVersionTestMock(mockJSON);
 
         //check data, it should be the same as the mock
-
+        assertTrue(test.getVersion()==1);
+        assertTrue(test.getAvatarList().get(0).getPath()=="https://www.yumesoftworks.com/fileshare/avatars/avatar_9.png");
     }
 }
